6c97a82ad38d4fe32ef62bfd896c1bd127e0e0b0
Harvey <118377154+mikezamayias@users.noreply.github.com>
2026-04-16 13:59:59 +0300

    Update lsom to 1.3.2

--- Casks/lsom.rb
+++ Casks/lsom.rb
@@ -1,9 +1,9 @@
 cask "lsom" do
-  version "1.2.0"
-  sha256 "46aca43e7d4b633948f2f6b6e9a9e687ca9298a7c81b0fc87391b1195356a9f3"
+  version "1.3.2"
+  sha256 "96e1d07a9cbc7c3d39eb94fd46da11f684a38d0212222decc5fb57b6884bcc78"
 
-  url "https://lsom-landing.pages.dev/downloads/lsom-v#{version}.dmg",
-      verified: "lsom-landing.pages.dev/"
+  url "https://github.com/mikezamayias/lsom/releases/download/v#{version}/lsom-v#{version}.dmg",
+      verified: "github.com/mikezamayias/lsom/"
   name "lsom"
   desc "Logitech Status on Mac - Monitor Logitech device battery from your menu bar"
   homepage "https://lsom-landing.pages.dev"
